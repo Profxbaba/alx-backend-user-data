31e24986e4247a129493a07ff6d13acd2fe2c58e
Profxbaba <daddyslove41@gmail.com>
2024-08-10 16:15:12 +0000

    Basic authentication

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -7,10 +7,12 @@ from api.v1.views import app_views
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+
 @app.errorhandler(401)
 def unauthorized_error(error):
     """Handle 401 Unauthorized error."""
     return jsonify({"error": "Unauthorized"}), 401
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -5,6 +5,7 @@ from flask import abort, Blueprint
 
 app_views = Blueprint('app_views', __name__)
 
+
 @app_views.route('/api/v1/unauthorized', methods=['GET'])
 def unauthorized():
     """Endpoint that triggers a 401 Unauthorized error."""

b5b9992a8f34fcb07f0c3c9a5f3f0060e3dd021e
Profxbaba <daddyslove41@gmail.com>
2024-08-14 21:58:10 +0000

    Find user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,43 +1,70 @@
 #!/usr/bin/env python3
 """
-DB module for managing the database
+DB module for interacting with the database.
 """
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm.session import Session
-from user import Base, User
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
+from user import User
+from typing import TypeVar, Dict, Any
 
 
 class DB:
-    """DB class for interacting with the database"""
+    """DB class for interacting with the database."""
 
     def __init__(self) -> None:
-        """Initialize a new DB instance"""
-        self._engine = create_engine("sqlite:///a.db", echo=False)
-        Base.metadata.drop_all(self._engine)
-        Base.metadata.create_all(self._engine)
-        self.__session = None
+        """Initialize a new DB instance."""
+        self._engine = create_engine("sqlite:///a.db")
+        self._Session = sessionmaker(bind=self._engine)
 
-    @property
-    def _session(self) -> Session:
-        """Memoized session object"""
-        if self.__session is None:
-            DBSession = sessionmaker(bind=self._engine)
-            self.__session = DBSession()
-        return self.__session
-
-    def add_user(self, email: str, hashed_password: str) -> User:
-        """Add a new user to the database and return the User object
+    def add_user(self, email: str, hashed_password: str) -> TypeVar('User'):
+        """
+        Adds a new user to the database.
 
         Args:
             email (str): The user's email address.
             hashed_password (str): The user's hashed password.
 
         Returns:
-            User: The newly created User object.
+            User: The newly created user.
         """
+        session = self._Session()
         new_user = User(email=email, hashed_password=hashed_password)
-        self._session.add(new_user)
-        self._session.commit()
+        session.add(new_user)
+        session.commit()
+        session.refresh(new_user)
+        session.close()
         return new_user
+
+    def find_user_by(self, **kwargs: Dict[str, Any]) -> TypeVar('User'):
+        """
+        Finds the first user in the database that matches the criteria
+        provided by keyword arguments.
+
+        Args:
+            kwargs (dict): Arbitrary keyword arguments to filter users.
+
+        Returns:
+            User: The first user that matches the criteria.
+
+        Raises:
+            NoResultFound: If no user matches the criteria.
+            InvalidRequestError: If the query is invalid.
+        """
+        if not kwargs:
+            raise InvalidRequestError("No filter arguments provided")
+
+        session = self._Session()
+
+        try:
+            user = session.query(User).filter_by(**kwargs).first()
+            if user is None:
+                raise NoResultFound("No user found with the given parameters")
+            return user
+        except InvalidRequestError as e:
+            session.rollback()
+            raise e
+        finally:
+            session.close()
--- 0x03-user_authentication_service/main.py
+++ 0x03-user_authentication_service/main.py
@@ -2,14 +2,30 @@
 """
 Main file
 """
-
 from db import DB
-from user import User
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
 
 my_db = DB()
 
-user_1 = my_db.add_user("test@test.com", "SuperHashedPwd")
-print(user_1.id)
+# Add a user to the database
+user = my_db.add_user("test@test.com", "PwdHashed")
+print(user.id)  # Should print the ID of the newly created user
 
-user_2 = my_db.add_user("test1@test.com", "SuperHashedPwd1")
-print(user_2.id)
+# Find the user by email
+find_user = my_db.find_user_by(email="test@test.com")
+print(find_user.id)  # Should print the same ID as above
+
+# Try to find a non-existing user by email
+try:
+    find_user = my_db.find_user_by(email="test2@test.com")
+    print(find_user.id)
+except NoResultFound:
+    print("Not found")  # Should print "Not found"
+
+# Try to find a user with an invalid filter
+try:
+    find_user = my_db.find_user_by(no_email="test@test.com")
+    print(find_user.id)
+except InvalidRequestError:
+    print("Invalid")  # Should print "Invalid"

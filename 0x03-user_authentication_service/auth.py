4a8acb6a296a453aef3b2b185d52046fd1fe7584
Profxbaba <daddyslove41@gmail.com>
2024-08-16 20:14:58 +0000

    Hash password

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -5,9 +5,10 @@ This module provides authentication methods, including password hashing.
 
 import bcrypt
 
+
 def _hash_password(password: str) -> bytes:
     """
-    Hashes a password string using bcrypt and returns the hashed password as bytes.
+    Hashes a password string using bcrypt & returns hashed password as bytes.
 
     Args:
         password (str): The password to hash.
